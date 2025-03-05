2b0b04377b8647df37bb2d573ec96912b9472bfc
Reynaldi Arya <reynaldiarya.id@gmail.com>
2025-03-05 10:24:33 +0700

    Initial commit

---
+++ src/components/Biolink.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <h1 class="text-4xl font-semibold mb-12">Your Name</h1>
+        <ul class="space-y-4">
+            <li v-for="(link, index) in links" :key="index">
+                <a :href="link.url" target="_blank" rel="noopener noreferrer"
+                    class="flex items-center justify-center w-96 h-12 bg-blue-500 text-white rounded-sm hover:bg-blue-600 transition duration-300 animate__animated animate__fadeIn">
+                    <font-awesome-icon :icon="link.icon" class="mr-1" /> {{ link.name }}
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import biolink from '@/collections/biolink.json'; export default {
+    data() {
+        return {
+            links: biolink,
+        };
+    },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+import './assets/main.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import 'animate.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+library.add(fab)
+createApp(App)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount('#app')
