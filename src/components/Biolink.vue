728f204364856fd990c0ead35a9bad304c9a1892
Reynaldi Arya <reynaldiarya.id@gmail.com>
2025-03-28 13:17:30 +0700

    v0.0.1 - Fix

--- src/components/Biolink.vue
+++ src/components/Biolink.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen flex flex-col items-center justify-center font-display bg-gray-100 px-4">
+    <div class="min-h-screen flex flex-col items-center justify-center font-display bg-gray-100 px-4 pt-16 pb-8">
         <img src="/assets/images/logo.png" class="w-32 h-32 rounded-full mb-6" alt="Logo">
         <h1 class="text-3xl font-semibold mb-12 text-center">Your Name</h1>
         <ul class="space-y-4 w-full max-w-xl">
@@ -10,6 +10,7 @@
                 </a>
             </li>
         </ul>
+        <p class="w-full text-center pt-12">Made with ♥ in Tulungagung</p>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import './assets/main.css';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import 'animate.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 library.add(fab)
+library.add(fas)
 createApp(App)
     .component("font-awesome-icon", FontAwesomeIcon)
     .mount('#app')
