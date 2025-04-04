c177432bc80f57d3f8e9ce411044f50899ba1416
Reynaldi Arya <reynaldiarya.id@gmail.com>
2025-04-04 13:30:31 +0700

    Fix Pagespeed

--- src/components/Biolink.vue
+++ src/components/Biolink.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="min-h-screen flex flex-col items-center justify-center font-display bg-gray-100 px-4 pt-16 pb-8">
-        <img src="/assets/images/logo.png" class="w-32 h-32 rounded-full mb-6" alt="Logo">
+        <img src="/assets/images/logo.png" class="w-32 h-32 rounded-full mb-6" alt="Avatar" loading="eager"
+            fetchpriority="high">
         <h1 class="text-3xl font-semibold mb-12 text-center">Your Name</h1>
         <ul class="space-y-4 w-full max-w-xl">
             <li v-for="(link, index) in links" :key="index">
                 <a :href="link.url" target="_blank" rel="noopener noreferrer"
-                    class="flex items-center justify-center w-full h-12 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300 shadow-md">
+                    class="flex items-center justify-center w-full h-12 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-300 shadow-md">
                     <font-awesome-icon :icon="link.icon" class="mr-2" /> {{ link.name }}
                 </a>
             </li>
