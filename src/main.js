72105d25b1fc96b372df0e9f212a63e1323d2401
Reynaldi Arya <reynaldiarya.id@gmail.com>
2025-04-04 20:09:17 +0700

    Fix huge javascript after build
    - Import only the required Font Awesome icons

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import './assets/main.css';
+import "./assets/main.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faXTwitter,
+  faTelegram,
+} from "@fortawesome/free-brands-svg-icons";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-library.add(fab)
-library.add(fas)
-createApp(App)
-    .component("font-awesome-icon", FontAwesomeIcon)
-    .mount('#app')
+library.add(faFacebook, faInstagram, faXTwitter, faTelegram, faBagShopping);
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
